f74ea90aea2045ac67c96ad377c037b175d85071
sudhiksha-ui <64886428+sudhiksha-ui@users.noreply.github.com>
2020-07-12 10:54:08 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,20 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+this.smoke = loadImage("sprites/smoke.png")
+    this.path=[]
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if(this.body.velocity.x>8&&this.body.position.x>210){
+var position=[this.body.position.x,this.body.position.y]
+this.path.push(position)}
+for(var i=0;i<this.path.length;i++){
+  image(this.smoke,this.path[i][0],this.path[i][1])
+}
+    super.display();
+  }
+}
